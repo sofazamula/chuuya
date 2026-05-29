4876ad44218a418c637341539c546d3e460621d9
sofazamula <sofazamula@gmail.com>
2026-05-29 11:02:31 +0500

    Add files via upload

---
+++ dogs.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githack.com/sofazamula/chuuya/main/453e7469bfcf151567484cef6b56f2c9.jpg",
+
+  "https://raw.githack.com/sofazamula/chuuya/main/c4f821550551b0891ac6db878456eef8.jpg",
+
+  "https://raw.githack.com/sofazamula/chuuya/main/c7e1301f63148f9ea1010440591e2471.jpg",
+
+  "https://raw.githack.com/sofazamula/chuuya/main/05c64dfaaa36719f7961557ebd4a5da4.jpg",
+
+  "https://raw.githack.com/sofazamula/chuuya/main/61033436490c2eb7715df8f9829a3488.jpg",
+
+  "https://raw.githack.com/sofazamula/chuuya/main/abba550810416f495aaa8b37c7dfee4c.jpg"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "NAKAJIMA ATSUSHI",
+
+  "DAZAI OSAMU",
+
+  "CHUUYA NAKAHARA",
+
+  "AKUTAGAWA RYUNOSUKE",
+
+  "FYODOR DOSTOEVSKY",
+
+  "MICHIZO TACHIHARA"
+
+];
